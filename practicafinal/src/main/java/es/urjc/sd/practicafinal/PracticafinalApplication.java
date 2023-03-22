d8c7283ba550cabd021c165ecd44e2ddac943d13
David Julián Largo <115720002+DeJulians@users.noreply.github.com>
2023-03-22 11:30:27 +0100

    Creacion del proyecto

---
+++ practicafinal/src/main/java/es/urjc/sd/practicafinal/PracticafinalApplication.java
@@ -0,0 +1,13 @@
+package es.urjc.sd.practicafinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticafinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticafinalApplication.class, args);
+	}
+
+}
