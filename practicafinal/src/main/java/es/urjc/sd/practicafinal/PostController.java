3159acb1f54c89785da24a50c16a36fef0935767
David Julián Largo <115720002+DeJulians@users.noreply.github.com>
2023-04-08 00:22:01 +0200

    .

--- practicafinal/src/main/java/es/urjc/sd/practicafinal/PostController.java
+++ practicafinal/src/main/java/es/urjc/sd/practicafinal/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 //import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.PutMapping;
