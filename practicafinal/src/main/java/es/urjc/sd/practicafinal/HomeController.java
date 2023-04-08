0fd06262bdfcec2449707384f4a633ab430e085d
IzanCruz <izancruzruiz@gmail.com>
2023-04-08 19:48:27 +0200

    Cambios varios para reducir código HTML
    He hecho varios cambios en el HTML, pero sigue sin mostrarse la información necesaria. Solo se muestra la parte del formulario de compra

--- practicafinal/src/main/java/es/urjc/sd/practicafinal/HomeController.java
+++ practicafinal/src/main/java/es/urjc/sd/practicafinal/HomeController.java
@@ -9,8 +9,7 @@ public class HomeController {
     
    
 
-    @GetMapping("/")
-    public String photos(Model model) {
+    private ClubSession[] getSessions() {
         ClubSession[] sessions = new ClubSession[] {
             new ClubSession(1, "REMEMBER","13/05 23:00h","Sala 1"),
             new ClubSession(2, "TECHNO","14/05 23:30" ,"Sala 2"),
@@ -18,22 +17,40 @@ public class HomeController {
             new ClubSession(4, "REGGAETON","06/05 23:30" ,"Sala 2"),
             new ClubSession(5, "MÚSICA ESPAÑOLA","13/05 23:00" ,"Sala 2"),
             new ClubSession(6, "POP","13/05 23:30" ,"Sala 2"),
-        }; 
+        };
+        return sessions;
+    }
+    
+    @GetMapping("/")
+    public String home(Model model) {
+        ClubSession[] sessions = getSessions();
         model.addAttribute("sessions", sessions);
         return "home";
+    }
+
+    @GetMapping("/entradas")
+    public String entradas(Model model) {
+        ClubSession[] sessions = getSessions();
+        model.addAttribute("sessions", sessions);
+        return "entradas";
     }
 
 
     @GetMapping("/login.html")
     public String login(Model model) {
-       
         return "login";
     }
+
     @GetMapping("/home.html")
     public String back(Model model) {
-       
         return "home";
     }
+
+    @GetMapping("/entradas.html") 
+    public String vistaentradas(Model model){
+        return "entradas";
+    }
+    
     
  
 
