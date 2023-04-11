b614e5dad7cffe334851e576a7615cff36f83c4e
Campaa02 <pca6@hotmail.es>
2023-04-11 11:30:32 +0200

    cambios en controladores y en estilos

---
+++ practicafinal/src/main/java/es/urjc/sd/practicafinal/EventAPIController.java
@@ -0,0 +1,33 @@
+package es.urjc.sd.practicafinal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+public class EventAPIController {
+
+ @Autowired
+     private EventService eventService; 
+
+    
+    @PostMapping("/api/events")
+	public ResponseEntity<Event> createEvent(@PathVariable Long id, @RequestBody Event event) {
+
+		event.setId(id);
+
+		if(eventService.save(event)){
+			return new ResponseEntity<>(event, HttpStatus.CREATED);
+		}else{
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+	}
+    }
+ 
+
