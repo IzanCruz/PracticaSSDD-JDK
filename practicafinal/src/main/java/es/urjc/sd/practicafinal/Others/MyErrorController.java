dbeae1f442e844904704361b5c96ce376135dda9
David Julián Largo <115720002+DeJulians@users.noreply.github.com>
2023-04-26 17:00:32 +0200

    .

--- practicafinal/src/main/java/es/urjc/sd/practicafinal/Others/MyErrorController.java
+++ practicafinal/src/main/java/es/urjc/sd/practicafinal/Others/MyErrorController.java
@@ -1,3 +1,11 @@
+package es.urjc.sd.practicafinal.Others;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class MyErrorController implements ErrorController {
 
@@ -7,7 +15,7 @@ public class MyErrorController implements ErrorController {
         return "myErrorPage";
     }
 
-    @Override
+    
     public String getErrorPath() {
         return "/error";
     }
