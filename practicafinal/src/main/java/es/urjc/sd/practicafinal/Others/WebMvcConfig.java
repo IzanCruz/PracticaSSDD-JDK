5f17c8f4e102d2cbf8baae57a7fdbd13627db174
David Julián Largo <115720002+DeJulians@users.noreply.github.com>
2023-04-26 14:01:59 +0200

    Add files via upload

---
+++ practicafinal/src/main/java/es/urjc/sd/practicafinal/Others/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package es.urjc.sd.practicafinal.Others;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/admin").setViewName("admin");
+        registry.addViewController("/user").setViewName("user");
+    }
+
+}
+
