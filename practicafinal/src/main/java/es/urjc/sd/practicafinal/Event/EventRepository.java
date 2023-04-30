88e9ad77cf13caf19a247102bef613fac358d2a2
David Julián Largo <115720002+DeJulians@users.noreply.github.com>
2023-04-30 23:25:02 +0200

    .

--- practicafinal/src/main/java/es/urjc/sd/practicafinal/Event/EventRepository.java
+++ practicafinal/src/main/java/es/urjc/sd/practicafinal/Event/EventRepository.java
@@ -1,12 +1,9 @@
 package es.urjc.sd.practicafinal.Event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List; 
 
 
-@Repository
 public interface EventRepository extends JpaRepository <Event, String> {
     List<Event> findByName(String name);
 
